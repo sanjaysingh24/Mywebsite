b8241e7ef4122dab9ec71cabb9507a5e835201e7
sanju <sanjubora84@gmail.com>
2024-03-16 11:31:10 +0530

    make changes

--- index.js
+++ index.js
@@ -17,36 +17,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/',(req,res)=>{
     res.render('index.ejs');
 })
-app.post('/submit',(req,res)=>{
+app.post('/submit',async (req,res)=>{
    const{name,email,subject,message} = req.body;
- console.log(email);
+
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
         user: process.env.USER,  // Your Gmail email address
         pass: process.env.PASSWORD       // Your Gmail password or app-specific password
     }
+
     
 });
-const mailOptions = {
-    from: email, // Sender's email address
-    name:name,   
-    to: process.env.USER,     // Recipient's email address
-    subject: subject,
-    text: message,
-};
+const info = await transporter.sendMail({
+    from: email, // sender address
+    to: "sanjubora84@gmail.com", // list of receivers
+    subject: subject, // Subject line
+    text: message, // plain text body
+ 
+  });
 
 
-transporter.sendMail(mailOptions, (error, info) => {
-    
-    if (error) {
-        console.error('Error sending email:', error);
-    } else {
-       
-        res.render('index.ejs',{message:"✅Email send Successfully"})
-    }
-});
+  console.log("Message sent: %s", info.messageId);
 })
 
  app.listen(port,()=>{
